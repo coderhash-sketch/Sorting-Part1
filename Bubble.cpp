5aa50cdf9de6c048d5384e398e80b62773ac85a1
coderhash-sketch <agrimgrg06@gmail.com>
2026-02-03 23:29:44 +0530

    Add files via upload

---
+++ Bubble.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+void swap(int &a,int &b){
+    int temp=a;
+    a=b;
+    b=temp;
+}
+void bubble_sort(int arr[],int n){
+    for(int i=n-1; i>=1; i--){
+        int didSwap=0;
+        for(int j=0;j<=i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                didSwap = 1;
+            }
+        }
+        if(didSwap==0){
+            break;
+        }
+        cout<<"runs\n";
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    bubble_sort(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
